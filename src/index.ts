88dd386e766051f29d0934ff2d25f60f85678a2d
llp-sudo <leonardo_lucasp@hotmail.com>
2020-10-29 16:29:31 -0300

    Desenvolvimento do Menu

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,67 @@
+//Imports padrão para desenvolver nova extenssão
 import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-/**
- * Initialization data for the Exodus extension.
- */
+//imports para desenvolver menu
+import { IMainMenu } from '@jupyterlab/mainmenu';
+
+import { Menu } from '@lumino/widgets';
+
+import { ICommandPalette } from '@jupyterlab/apputils';
+
+// informações da extensão
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'Exodus',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension Exodus is activated!');
+  requires: [ICommandPalette, IMainMenu],
+  //função executada ao cliclar na extensão
+  activate: (
+    app: JupyterFrontEnd,
+    palette: ICommandPalette,
+    mainMenu: IMainMenu
+  ) => {
+    const { commands } = app;
+
+    // Add a command
+    const command = 'Exodus-interface';
+    //adiciona um comando ao clickar no menu
+    commands.addCommand(command, {
+      //nome subm-menu
+      label: 'Funções em Python!',
+      caption: 'Funções em Python',
+      //Comando do click
+      execute: (args: any) => {
+        window.alert(
+          `Chamada de Input para funções Python`
+        );
+      }
+    });
+   
+
+    // Add the command to the command palette
+    const category = 'Extension Exodus';
+    palette.addItem({
+      command,
+      category,
+      args: { origin: 'from the palette' }
+    });
+
+
+    // Create a menu
+    const Exodus: Menu = new Menu({ commands });
+    //nome menu principal
+    Exodus.title.label = 'Exodus';
+    mainMenu.addMenu(Exodus, { rank: 80 });
+
+    // Add the command to the menu
+    Exodus.addItem({ command, args: { origin: 'from the menu' } });
+
   }
+
+
+
 };
 
 export default extension;
